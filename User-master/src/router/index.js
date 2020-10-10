5444baf2904231a8c81533c2e1f028f21f970646
Luqian <1791502895@qq.com>
2020-10-10 09:01:06 +0800

    111

--- User-master/src/router/index.js
+++ User-master/src/router/index.js
@@ -3,45 +3,58 @@ import VueRouter from 'vue-router'
 const Home = () => import('../views/home/Home.vue')
 const Hxzz = () => import('../views/hxzz/Hxzz.vue')
 
+const SaferunInfoManage = () => import('../../src/views/saferuninfo/SaferunInfoManage.vue')
+const TransferInfoManage = () => import('../../src/views/transferinfo/TransferInfoManage.vue')
+
 const ComponyInfoManage = () => import('../../src/views/componyinfo/ComponyInfoManage.vue')
 const PostInfoManage = () => import('../../src/views/postinfo/PostInfoManage.vue')
 const OrderInfoManage = () => import('../../src/views/orderinfo/OrderInfoManage.vue')
 const PostType = () => import('../views/datadictionary/DataDictionary.vue')
 Vue.use(VueRouter)
-
+//定义路由
   const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Hxzz',
+  //   component: Hxzz,
+  // },
+  // {
+  // 	path:'/',
+  // 	name:'SaferunInfoManage',
+  // 	component:SaferunInfoManage,
+  // },
   {
-    path: '/',
-    name: 'Hxzz',
-    component: Hxzz
+  	path:'/',
+  	name:'TransferInfoManage',
+  	component:TransferInfoManage,
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home,
-    children:[
-      {
-        path: '/componyinfomanage',
-        name: '企业信息',
-        component: ComponyInfoManage,
-      },
-      {
-        path: '/postinfomange',
-        name: '岗位信息',
-        component: PostInfoManage,
-      },
-      {
-        path: '/orderinfomange',
-        name: '订单信息',
-        component: OrderInfoManage,
-      },
-      {
-        path: '/datamangeinfo',
-        name: '订单信息',
-        component: PostType,
-      },
-    ]
-  },
+  // {
+  //   path: '/home',
+  //   name: 'Home',
+  //   component: Home,
+  //   children:[
+  //     {
+  //       path: '/componyinfomanage',
+  //       name: '企业信息',
+  //       component: ComponyInfoManage,
+  //     },
+  //     {
+  //       path: '/postinfomange',
+  //       name: '岗位信息',
+  //       component: PostInfoManage,
+  //     },
+  //     {
+  //       path: '/orderinfomange',
+  //       name: '订单信息',
+  //       component: OrderInfoManage,
+  //     },
+  //     {
+  //       path: '/datamangeinfo',
+  //       name: '订单信息',
+  //       component: PostType,
+  //     },
+  //   ]
+  // },
 ]
 
 const router = new VueRouter({

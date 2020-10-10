17862c85c0e5e33f99d5d35ec00dfad46ae2d446
Luqian <1791502895@qq.com>
2020-10-10 09:09:34 +0800

    second new file

---
+++ User-master/src/views/saferuninfo/SaferunInfoManage.vue
@@ -0,0 +1,119 @@
+<template>
+	<div id="Sa">
+		<!-- 头部盒子 -->
+		<header>
+			<h1>SGMW河西基地总装车间</h1>
+			<div class="show-head">匠心创造 装配可调</div>
+			<!--时间显示-->
+			<div class="show-time">{{getdate()}}</div>
+		</header>
+		<!--main盒子主体部分-->
+		<div class="mainbox">
+			<!-- 左側盒子 -->
+			<div class="column">
+				<div class="panel-left">
+					<div class="panel-left-small">
+						<h1><a href="#">安全运行</a></h1>						
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="scgl.html">生产管理</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="lzcsj.html">流转车数据</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="zl.html">质量</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="rygl.html">人员管理</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="nhgl.html">能耗管理</a></h1>
+					</div>
+					
+					<div class="panel-left-small">
+		                <h1><a href="sbzt.html">设备状态</a></h1>
+					</div>
+				</div>
+			</div>
+			<!-- main -->
+			<div  class="column">
+				
+				<!-- 安全运行页面开始 -->
+				<div class="panel-main" >
+					<el-row :gutter="20" type="flex"  class="row-b" justify="space-around">
+					  <el-col :span="6" class="row-c">
+						  <div class="grid-content bg-purple">损工事故：</div>
+						  <div class="inputsc"></div>
+						  </el-col>
+					  <el-col :span="6" class="row-c"><div class="grid-content bg-purple">可记录事故：</div></el-col>
+					  <el-col :span="6" class="row-c"><div class="grid-content bg-purple">急救事故：</div></el-col>
+					  <el-col :span="6" class="row-c"><div class="grid-content bg-purple">险肇事故</div></el-col>
+					</el-row>
+					<el-row :gutter="20" type="flex" class="row-bg" justify="space-around">
+					  <el-col :span="6"><div class="grid-content bg-purple">相关方事故：</div></el-col>
+					  <el-col :span="6"><div class="grid-content bg-purple">火险事故：</div></el-col>
+					  <el-col :span="6"><div class="grid-content bg-purple">环境事故：</div></el-col>
+					  
+					</el-row>
+				<!-- 安全绿十字 -->
+					    <div class="content">
+					        <div class="cross up"></div>
+					        <div class="cross left"></div>
+					        <div class="cross icon"></div>
+					        <div class="cross right"></div>
+					        <div class="cross down "></div>
+					    </div>
+
+										
+				</div>
+				<!-- 安全运行结束 -->
+				<!-- 底部盒子 -->
+				<div class="panel-main-footer">
+					<div class="show-button">
+							<button type="button" class="el-button--detal" onclick=javascript:jumpindex() >
+								<span>返回首页</span>
+							</button>													
+					</div>
+					<div class="btn-next">
+						
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				inputs:'',
+				inputk:''
+			}
+		},
+		methods:{
+			//时间显示
+			getdate() {
+				var date = new Date();
+				var seperatorl = "-";
+				var year = date.getFullYear();
+				var month = date.getMonth() + 1;
+				var strDate = date.getDate();
+				if (month >= 1 && month <= 9) {
+					month = "0" + month;
+				}
+				if (strDate >= 0 && strDate <= 9) {
+					strDate = "0" + strDate;
+				}
+				var currentdate = year + "年" + month + "月" + strDate + "日";
+				return currentdate;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+		@import "../../assets/css/aqyx.css";
+</style>
---
+++ User-master/src/views/transferinfo/TransferInfoManage.vue
@@ -0,0 +1,158 @@
+<template>
+	<div id="Sa">
+		<!-- 头部盒子 -->
+		<header>
+			<h1>SGMW河西基地总装车间</h1>
+			<div class="show-head">匠心创造 装配可调</div>
+			<!--时间显示-->
+			<div class="show-time">{{getdate()}}</div>
+		</header>
+		<!--main盒子主体部分-->
+		<div class="mainbox">
+			<!-- 左側盒子 -->
+			<div class="column">
+				<div class="panel-left">
+					<div class="panel-left-small">
+						<h1><a href="#">安全运行</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="scgl.html">生产管理</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="lzcsj.html">流转车数据</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="zl.html">质量</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="rygl.html">人员管理</a></h1>
+					</div>
+					<div class="panel-left-small">
+						<h1><a href="nhgl.html">能耗管理</a></h1>
+					</div>
+
+					<div class="panel-left-small">
+						<h1><a href="sbzt.html">设备状态</a></h1>
+					</div>
+				</div>
+			</div>
+			<!-- main -->
+			<div class="column">
+
+				<!-- 流转车数据页面开始 -->
+				<div class="panel-main">
+					<!-- <el-table :data="tableData.slice((currentPage4-1)*100,currentPage4*100)" style="width: 100%">
+						根据当前页码和每页需要展示的条数来展示数据 -->
+						
+					<el-table :data="tableData" style="width: 100%">
+						<el-table-column prop="date" label="" width="180">
+						</el-table-column>
+						<el-table-column prop="drk" label="待入库" width="180">
+						</el-table-column>
+						<el-table-column prop="phl" label="待过线跑坏路">
+						</el-table-column>
+						<el-table-column prop="bhlz" label="BH流转">
+						</el-table-column>
+						<el-table-column prop="sjlz" label="实际流转">
+						</el-table-column>
+						<el-table-column prop="hj" label="合计">
+						</el-table-column>
+					</el-table>
+
+				</div>
+				<div class="pag">
+				    <el-pagination
+				      @size-change="handleSizeChange"
+				      @current-change="handleCurrentChange"
+				      :current-page="currentPage4"
+				      :page-sizes="[100, 200, 300, 400]"
+				      :page-size="100"
+				      layout="total, sizes, prev, pager, next, jumper"
+				      :total="400">
+				    </el-pagination>
+				  </div>
+				 
+				<!-- 流转车数据结束 -->
+				<!-- 底部盒子 -->
+				<div class="panel-main-footer">
+				</div>
+				<div class="btn-next">
+				     <el-date-picker
+				       v-model="value1"
+				       type="daterange"
+				       range-separator="至"
+				       start-placeholder="开始日期"
+				       end-placeholder="结束日期">
+				     </el-date-picker>
+				   
+				 <el-button round>查看</el-button>
+				<el-button type="primary">返回首页</el-button>
+				 					</div>
+			</div>
+
+		</div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				 value1: '',
+				tableData: [{
+					date: 'CN120S',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}, {
+					date: 'CN180C',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}, {
+					date: 'CN180S',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}, {
+					date: 'CN185M',
+					drk: '12',
+					phl: '10',
+					bhlz:'9',
+					sjlz:'8',
+					hj:'10'
+				}]
+			}
+		},
+		methods: {
+			//时间显示
+			getdate() {
+				var date = new Date();
+				var seperatorl = "-";
+				var year = date.getFullYear();
+				var month = date.getMonth() + 1;
+				var strDate = date.getDate();
+				if (month >= 1 && month <= 9) {
+					month = "0" + month;
+				}
+				if (strDate >= 0 && strDate <= 9) {
+					strDate = "0" + strDate;
+				}
+				var currentdate = year + "年" + month + "月" + strDate + "日";
+				return currentdate;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	@import "../../assets/css/pub.css";
+	@import "../../assets/css/lzcsj.css";
+</style>
